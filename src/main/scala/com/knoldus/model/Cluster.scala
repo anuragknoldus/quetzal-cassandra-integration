1f4d73ceead109c472954da6f483e179011f762d
Charmy Garg <charmy.garg@knoldus.in>
2017-11-14 10:31:18 +0530

    Updated col_number for prop and val

--- src/main/scala/com/knoldus/model/Cluster.scala
+++ src/main/scala/com/knoldus/model/Cluster.scala
@@ -79,12 +79,11 @@ class Cluster {
   }
 
   def insertIntoDPH(id: UUID, entity: String, domain: String, col_number: Int, predicate_value: String, object_value: String): ExecutionInfo = {
-
     val session = createCluster().connect()
     try {
       val query = s"""Insert into $databaseName.$DPH($Id, $Entity, $Spill, $Domain, ${Prop + col_number}, ${Val + col_number}) values ($id, '$entity', 0, '$domain', '$predicate_value', '$object_value')"""
-      val rs = session.execute(query)
-      rs.getExecutionInfo
+      val resultSet = session.execute(query)
+      resultSet.getExecutionInfo
     } catch {
       case exception: Exception => throw exception
     } finally {
@@ -92,34 +91,46 @@ class Cluster {
     }
   }
 
-  def insertDPH(id: UUID, entity: String, domain: String, col_number: Int, predicate_value: String, object_value: String) = {
+  def upsertDPH(id: UUID, entity: String, domain: String, col_number: Int, predicate_value: String, object_value: String): ExecutionInfo = {
     val session = createCluster().connect()
-    val query = s"""Select id, entity from $databaseName.$DPH where entity = '$entity' ALLOW FILTERING"""
-
-    val rs = session.execute(query)
-    if (Option(rs.one()).isDefined) {
-      updateDPH(id, entity, domain, predicate_value, object_value, 1)
-    } else {
-      insertIntoDPH(id, entity, domain, col_number, predicate_value, object_value)
+    try {
+      val query = s"""Select id, entity from $databaseName.$DPH where entity = '$entity' ALLOW FILTERING"""
+      val resultSet = session.execute(query)
+      if (Option(resultSet.one()).isDefined) {
+        updateDPH(id, entity, domain, col_number, predicate_value, object_value, 1)
+      } else {
+        insertIntoDPH(id, entity, domain, col_number, predicate_value, object_value)
+      }
+      resultSet.getExecutionInfo
+    } catch {
+      case exception: Exception => throw exception
+    } finally {
+      session.close()
     }
-    session.close()
   }
 
-  def updateDPH(id: UUID, entity: String, domain: String, predicate_value: String, object_value: String, spill: Int) = {
+  def updateDPH(id: UUID, entity: String, domain: String, col_number: Int, predicate_value: String, object_value: String, spill: Int): ResultSet = {
     val session = createCluster().connect()
-    val propQuery = s"""Select id, prop1, val1 from $databaseName.$DPH where prop1 = '$predicate_value' and val1 = '$object_value' ALLOW FILTERING"""
-    val row = session.execute(propQuery).one
-    val query = if (Option(row).isDefined) {
-      val persistedId = row.getUUID("id")
-      val updateQuery = s"""update $databaseName.$DPH set spill = $spill where domain = '$domain' and id = $persistedId"""
-      session.execute(updateQuery)
-      s"""insert into $databaseName.$DPH($Id, $Entity, $Spill, $Domain) values ($id, '$entity', $spill, '$domain')"""
-    } else {
-      val id = row.getUUID("id")
-      s"""update $databaseName.$DPH set prop1 = '$predicate_value' and val1 = '$object_value' where domain = '$domain' and id = $id"""
+    try {
+      val propQuery =
+        s"""Select id, prop$col_number, val$col_number from $databaseName.$DPH where prop$col_number =
+           |'$predicate_value' and val$col_number = '$object_value' ALLOW FILTERING""".stripMargin
+      val propRow = session.execute(propQuery).one
+      val query = if (Option(propRow).isDefined) {
+        val persistedId = propRow.getUUID("id")
+        val updateQuery = s"""update $databaseName.$DPH set spill = $spill where domain = '$domain' and id = $persistedId"""
+        session.execute(updateQuery)
+        s"""insert into $databaseName.$DPH($Id, $Entity, $Spill, $Domain) values ($id, '$entity', $spill, '$domain')"""
+      } else {
+        val id = propRow.getUUID("id")
+        s"""update $databaseName.$DPH set prop$col_number = '$predicate_value' and val$col_number = '$object_value' where domain = '$domain' and id = $id"""
+      }
+      session.execute(query)
+    } catch {
+      case exception: Exception => throw exception
+    } finally {
+      session.close()
     }
-    session.execute(query)
-    session.close()
   }
 
 }
@@ -128,12 +139,10 @@ class Cluster {
 object Clusters {
   def main(args: Array[String]): Unit = {
     val cluster = new Cluster
-    /*cluster.createDatabase()
-    cluster.createPredicateTable()*/
-    println(cluster.createDPHTable())
+    cluster.createDatabase()
+    cluster.createPredicateTable()
     val uuid = java.util.UUID.randomUUID
-    println(uuid + " -d")
-    cluster.insertDPH(uuid, "Sodium", "Chemistry", 1, "Property1", "Value1")
+    cluster.upsertDPH(uuid, "Sodium", "Chemistry", 1, "Property1", "Value1")
   }
 
 }
